7e3073ec509f13107b145ddfab9e6b724c263004
10cyrilc <10cyrilc@gmail.com>
2025-01-25 00:34:14 +0530

    Add JWT authentication to AuthService and update DTO exports

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,10 +7,16 @@ import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto, SignupDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
   async login(dto: LoginDto) {
     const user = await this.prisma.user.findFirst({
       where: {
@@ -28,8 +34,7 @@ export class AuthService {
       throw new ForbiddenException('Invalid credentials');
     }
 
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
   async signup(dto: SignupDto) {
@@ -44,8 +49,7 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -54,5 +58,20 @@ export class AuthService {
         throw error;
       }
     }
+  }
+
+  async signToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const access_token = await this.jwtService.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.configService.get<string>('JWT_SECRET'),
+    });
+
+    // TODO: Implement refresh token.
+
+    return { access_token };
   }
 }
