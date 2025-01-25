712b2aef4b599af2830771ae70339518dddbacf3
10cyrilc <10cyrilc@gmail.com>
2025-01-25 20:22:29 +0530

    Add end-to-end tests for authentication and user retrieval

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,11 @@
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { LoginDto, SignupDto } from '../src/auth/dto';
+
+const BASE_URL = 'http://localhost:3300';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -24,10 +28,12 @@ describe('App e2e', () => {
     );
 
     await app.init();
+    await app.listen(3300);
 
     // Get the PrismaService instance from the app container and clean the database.
     primsa = app.get<PrismaService>(PrismaService);
     await primsa.cleanDb();
+    pactum.request.setBaseUrl(BASE_URL);
   });
 
   /**
@@ -39,5 +45,109 @@ describe('App e2e', () => {
     await app.close();
   });
 
-  describe('Auth', () => {});
+  describe('Auth', () => {
+    describe('Sign Up', () => {
+      it('Should throw error, email is empty', () => {
+        const signUpDto: SignupDto = {
+          email: '',
+          password: '123',
+          firstName: 'test',
+          lastName: 'user',
+        };
+        return pactum
+          .spec()
+          .post(`/auth/sign-up`)
+          .withBody(signUpDto)
+          .expectStatus(400);
+      }, 5000);
+      it('should signup and return a JWT token', () => {
+        const signUpDto: SignupDto = {
+          email: 'test@test.com',
+          password: '123',
+          firstName: 'test',
+          lastName: 'user',
+        };
+        return pactum
+          .spec()
+          .post(`/auth/sign-up`)
+          .withBody(signUpDto)
+          .expectStatus(201);
+      }, 5000);
+      it('Should throw email already exists', () => {
+        const signUpDto: SignupDto = {
+          email: 'test@test.com',
+          password: '123',
+          firstName: 'test',
+          lastName: 'user',
+        };
+        return pactum
+          .spec()
+          .post(`/auth/sign-up`)
+          .withBody(signUpDto)
+          .expectStatus(403);
+      }, 5000);
+    });
+
+    describe('Sign In', () => {
+      it('Should throw error no email', () => {
+        const loginDto: LoginDto = {
+          email: '',
+          password: '123',
+        };
+        return pactum
+          .spec()
+          .post(`/auth/login`)
+          .withBody(loginDto)
+          .expectStatus(400);
+      }, 5000);
+      it('Should throw error no email', () => {
+        const loginDto: LoginDto = {
+          email: 'test@test.com',
+          password: '',
+        };
+        return pactum
+          .spec()
+          .post(`/auth/login`)
+          .withBody(loginDto)
+          .expectStatus(400);
+      }, 5000);
+
+      it('should login and return a JWT token', () => {
+        const loginDto: LoginDto = {
+          email: 'test@test.com',
+          password: '123',
+        };
+        return pactum
+          .spec()
+          .post(`/auth/login`)
+          .withBody(loginDto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      }, 5000);
+    });
+  });
+  describe('User', () => {
+    describe('Get Me', () => {
+      it('Should return user details', () => {
+        return pactum
+          .spec()
+          .get(`/users/me`)
+          .withBearerToken(`$S{userAt}`)
+          .expectStatus(200);
+      }, 5000);
+    });
+
+    describe('Edit me', () => {});
+  });
+  describe('Bookmarks', () => {
+    describe('Create Bookmark', () => {});
+
+    describe('Get All Bookmarks', () => {});
+
+    describe('Get a Bookmark by ID', () => {});
+
+    describe('Edit Bookmark by Id', () => {});
+
+    describe('Delete Bookmark by ID', () => {});
+  });
 });
