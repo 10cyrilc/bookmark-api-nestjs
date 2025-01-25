1f4cfce992c60abc65df83b5a84fb6c688e368d5
10cyrilc <10cyrilc@gmail.com>
2025-01-25 23:40:44 +0530

    Implement Bookmark functionality with CRUD operations and DTOs

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { BookmarkService } from './bookmark.service';
+import { GetUser } from '../auth/decorator';
+import { CreateBookMarkDto, EditBookMarkDto } from './dto';
 
-@Controller('bookmark')
-export class BookmarkController {}
+@UseGuards(JwtGuard)
+@Controller('bookmarks')
+export class BookmarkController {
+  constructor(private bookMarkService: BookmarkService) {}
+  @Post()
+  createBookmark(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateBookMarkDto,
+  ) {
+    return this.bookMarkService.createBookmark(userId, dto);
+  }
+
+  @Get()
+  getBookmarks(@GetUser('id') userId: number) {
+    return this.bookMarkService.getBookmarks(userId);
+  }
+
+  @Get(':id')
+  getBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookMarkService.getBookmarkById(userId, bookmarkId);
+  }
+
+  @Patch(':id')
+  editBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() dto: EditBookMarkDto,
+  ) {
+    return this.bookMarkService.editBookmarkById(userId, bookmarkId, dto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookMarkService.deleteBookmarkById(userId, bookmarkId);
+  }
+}
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateBookMarkDto, EditBookMarkDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  constructor(private prismaService: PrismaService) {}
+  async createBookmark(userId: number, dto: CreateBookMarkDto) {
+    const bookmark = await this.prismaService.bookmark.create({
+      data: {
+        userId,
+        ...dto,
+      },
+    });
+    return bookmark;
+  }
+
+  getBookmarks(userId: number) {
+    return this.prismaService.bookmark.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  getBookmarkById(userId: number, bookmarkId: number) {
+    return this.prismaService.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+        userId,
+      },
+    });
+  }
+
+  async editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    dto: EditBookMarkDto,
+  ) {
+    const bookmark = await this.prismaService.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    if (!bookmark) {
+      throw new NotFoundException('Bookmark not found');
+    }
+
+    if (bookmark.userId !== userId) {
+      throw new ForbiddenException('Access to this resource is forbidden');
+    }
+
+    return this.prismaService.bookmark.update({
+      where: {
+        id: bookmarkId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteBookmarkById(userId: number, bookmarkId: number) {
+    const bookmark = await this.prismaService.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    if (!bookmark) {
+      throw new NotFoundException('Bookmark not found');
+    }
+
+    if (bookmark.userId !== userId) {
+      throw new ForbiddenException('Access to this resource is forbidden');
+    }
+
+    await this.prismaService.bookmark.delete({
+      where: {
+        id: bookmarkId,
+      },
+    });
+  }
+}
